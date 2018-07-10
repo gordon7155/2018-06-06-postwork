f68efe0dc10fcd95d8070c9ee953f00c66959d39
gordon7155 <krystian.gordziejewski@gmail.com>
2018-07-10 12:27:16 +0200

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -23,40 +23,111 @@ Rozwiązania wysyłamy tak samo, jak prework, tylko że w jednym Pull Requeście
 import cmd, sys
 import turtle
 
+
 class TurtleShell(cmd.Cmd):
     intro = 'Welcome to the turtle shell.   Type help or ? to list commands.\n'
     prompt = '(turtle) '
 
-    # ----- basic turtle commands -----
+    def __init__(self):
+        super().__init__()
+        self.macro = Invoker()
+        self.record = False
+
+
+# ----- basic turtle commands -----
     def do_forward(self, arg):
         'Move the turtle forward by the specified distance:  FORWARD 10'
-        turtle.forward(int(arg))
+        if self.record:
+            self.macro.add(Command(turtle.forward, arg))
+        else:
+            turtle.forward(int(arg))
+
     def do_right(self, arg):
         'Turn turtle right by given number of degrees:  RIGHT 20'
-        turtle.right(int(arg))
+        if self.record:
+            self.macro.add(Command(turtle.right, arg))
+        else:
+            turtle.right(int(arg))
+
     def do_left(self, arg):
         'Turn turtle left by given number of degrees:  LEFT 90'
-        turtle.left(int(arg))
+        if self.record:
+            self.macro.add(Command(turtle.left, arg))
+        else:
+            turtle.left(int(arg))
+
     def do_home(self, arg):
         'Return turtle to the home position:  HOME'
-        turtle.home()
+        if self.record:
+            self.macro.add(Command(turtle.home, arg))
+        else:
+            turtle.home()
+
     def do_circle(self, arg):
         'Draw circle with given radius an options extent and steps:  CIRCLE 50'
-        turtle.circle(int(arg))
+        if self.record:
+            self.macro.add(Command(turtle.circle, arg))
+        else:
+            turtle.circle(int(arg))
+
     def do_position(self, arg):
         'Print the current turtle position:  POSITION'
         print('Current position is %d %d\n' % turtle.position())
+
+
     def do_heading(self, arg):
         'Print the current turtle heading in degrees:  HEADING'
         print('Current heading is %d\n' % (turtle.heading(),))
+
+
     def do_reset(self, arg):
         'Clear the screen and return turtle to center:  RESET'
         turtle.reset()
+
     def do_bye(self, arg):
         'Close the turtle window, and exit:  BYE'
         print('Thank you for using Turtle')
         turtle.bye()
         return True
 
+    def do_record(self, arg):
+        print('Started recording')
+        self.record = True
+
+    def do_stop(self, arg):
+        print('Stopped recording')
+        self.record = False
+
+    def do_playback(self, arg):
+        if self.record is True:
+            print('Should stop recording first')
+        else:
+            self.macro.play()
+
+
+class Command:
+    def __init__(self, command, arg):
+        self.command = command
+        self.arg = arg
+
+    def execute(self):
+        if self.arg:
+            self.command(int(self.arg))
+        else:
+            self.command()
+
+
+class Invoker:
+    def __init__(self):
+        self.commands = []
+
+    def add(self, command):
+        self.commands.append(command)
+
+    def play(self):
+        for command in self.commands:
+            command.execute()
+
+
 if __name__ == '__main__':
-    TurtleShell().cmdloop()    
+    TurtleShell().cmdloop()
